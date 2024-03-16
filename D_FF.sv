a9e8bd2cc2fdcc5448619392329491caea594ac9
Sandeepan Roy <44930092+Sandeepan26@users.noreply.github.com>
2024-03-17 03:34:36 +0530

    Added D Flip Flop RTL code
    This is a design of a  positive edge triggered D  Flip-Flop

---
+++ D_FF.sv
@@ -0,0 +1,16 @@
+module dff(input clk, d, rst, output reg q);
+  
+  always@(posedge clk) begin
+    if(rst)
+      q <= 1'b0;
+    else
+      q <= d;
+  end
+  
+endmodule
+
+/*--INTERFACE------ */
+
+interface dff_intf();
+  logic clk, d, rst, q;
+endinterface
