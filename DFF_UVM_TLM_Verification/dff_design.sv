7cc4ddb4feb4b35f9ab8c3e24c770941243a31b0
Sandeepan Roy <44930092+Sandeepan26@users.noreply.github.com>
2024-08-31 03:51:19 +0530

    Update dff_design.sv

--- DFF_UVM_TLM_Verification/dff_design.sv
+++ DFF_UVM_TLM_Verification/dff_design.sv
@@ -1,1 +1,17 @@
 
+module dff(input clk, d, rst, output reg q);
+  
+  always@(posedge clk) begin
+    if(rst)
+      q <= 1'b0;
+    else
+      q <= d;
+  end
+  
+endmodule
+
+/*--INTERFACE------ */
+
+interface dff_intf();
+  logic clk, d, rst, q;
+endinterface
