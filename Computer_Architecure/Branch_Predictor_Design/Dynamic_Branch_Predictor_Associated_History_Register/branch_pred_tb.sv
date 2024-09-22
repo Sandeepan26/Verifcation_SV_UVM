f7feb558cfd400718c5bc895563a5ae5b8b8e13c
Sandeepan Roy <44930092+Sandeepan26@users.noreply.github.com>
2024-09-22 11:35:41 +0530

    Create branch_pred_tb.sv

---
+++ Computer_Architecure/Branch_Predictor_Design/Dynamic_Branch_Predictor_Associated_History_Register/branch_pred_tb.sv
@@ -0,0 +1,38 @@
+module tb;
+  
+  reg br;
+  reg [31:0] pc;
+  wire hit;
+  wire [31:0] prdbr;
+  
+  branch_predictor bprd(br, pc, hit, prdbr);
+  
+  initial begin
+    
+    br = 1'b1;
+    pc = 1024;
+    
+    #2
+    
+    pc = 1024;
+    
+    #2
+    br = 1'b0;
+    pc = 1025;
+    
+    #2
+    br = 1'b1;
+    pc = 1024;
+   
+    
+    #10 $finish(1);
+  end
+  
+  initial begin
+    
+    $dumpfile("signals_dump.vcd");
+    $dumpvars;
+    
+  end
+  
+endmodule
